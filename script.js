40f3f0ea4ca569e4ed5fdebae66ce75f70d3c698
Toussaint  Gilbert <toussaintgilbert@Toussaints-MacBook-Pro-2.local>
2023-07-08 15:58:29 -0500

    added local storage function

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 var date 
 date = document.querySelector('#currentDay')
-date.textContent = dayjs().format('dddd, MMMM D') 
+date.textContent = dayjs().format('dddd, MMMM D')
+var currentHour = dayjs().hour()
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 var timeBlocks
-document.querySelectorAll()
+timeBlocks = document.querySelectorAll('.time-block')
+for (let i = 0; i < timeBlocks.length; i++) {
+  console.log(timeBlocks[i]);
+  let hour = parseInt(timeBlocks[i].id.substring(5));
+  console.log(hour);
+
+  if (currentHour === hour) {
+    timeBlocks[i].classList.add('present');
+  } else if(currentHour < hour){
+    timeBlocks[i].classList.add('future');
+  } else if(currentHour > hour) {
+    timeBlocks[i].classList.add('past');
+  }
+  let textArea = timeBlocks[i].querySelector('.description');
+  textArea.value = localStorage.getItem(hour)
+  timeBlocks[i].querySelector('.saveBtn').addEventListener("click",function () {
+    console.log(textArea.value);
+    localStorage.setItem(hour, textArea.value);
+  })
+}
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
+
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
