e82bb7e72a690e5003c2e99ee504016952e40a01
Toussaint  Gilbert <toussaintgilbert@Toussaints-MacBook-Pro-2.local>
2023-07-08 15:16:42 -0500

    added current date/time

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+var date 
+date = document.querySelector('#currentDay')
+date.textContent = dayjs().format('dddd, MMMM D') 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var timeBlocks
+document.querySelectorAll()
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
